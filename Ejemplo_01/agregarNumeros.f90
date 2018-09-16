7b8b18b3852240753a51272a85ec6cee9a9e8f47
Adrián Ruiz <aerf2007@gmail.com>
2018-09-15 22:19:15 -0500

    Add 1 & 2 previous

---
+++ Ejemplo_01/agregarNumeros.f90
@@ -0,0 +1,11 @@
+program addNumbers
+	! Esto simplemente añade 2 numeros
+	implicit none
+	! Declaraciones
+	real :: a,b, result
+	! Ejecutando sentencias
+	a = 12.0
+	b = 15.0
+	result = a + b
+	print *, 'The total is ', result
+end program addNumbers
